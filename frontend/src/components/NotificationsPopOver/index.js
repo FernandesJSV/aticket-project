cc615a6ca736e404e43ca2e44f844fb3ce64b5eb
/codsec <44205014+codsec@users.noreply.github.com>
2023-04-12 18:53:06 -0400

    Fix Notification

--- frontend/src/components/NotificationsPopOver/index.js
+++ frontend/src/components/NotificationsPopOver/index.js
@@ -117,6 +117,14 @@ const NotificationsPopOver = () => {
 				!data.message.read &&
 				(data.ticket.userId === user?.id || !data.ticket.userId)
 			) {
+				const shouldNotNotificate =
+					(data.message.ticketId === ticketIdRef.current &&
+						document.visibilityState === "visible") ||
+					(data.ticket.userId && data.ticket.userId !== user?.id) ||
+					data.ticket.isGroup || user.queues.map(q => q.id).indexOf(data.ticket.queueId) === -1;
+
+				if (shouldNotNotificate) return;
+				
 				setNotifications(prevState => {
 					const ticketIndex = prevState.findIndex(t => t.id === data.ticket.id);
 					if (ticketIndex !== -1) {
@@ -126,13 +134,6 @@ const NotificationsPopOver = () => {
 					return [data.ticket, ...prevState];
 				});
 
-				const shouldNotNotificate =
-					(data.message.ticketId === ticketIdRef.current &&
-						document.visibilityState === "visible") ||
-					(data.ticket.userId && data.ticket.userId !== user?.id) ||
-					data.ticket.isGroup || user.queues.map(q => q.id).indexOf(data.ticket.queueId) === -1;
-
-				if (shouldNotNotificate) return;
 
 				handleNotifications(data);
 			}
