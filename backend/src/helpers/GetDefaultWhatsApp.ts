34793c476f73bc0916a6314db6c9a81abe00a692
/codsec <44205014+codsec@users.noreply.github.com>
2023-04-13 13:13:53 -0300

    Check Wpp connection userID or CompanyID

--- backend/src/helpers/GetDefaultWhatsApp.ts
+++ backend/src/helpers/GetDefaultWhatsApp.ts
@@ -3,21 +3,24 @@ import Whatsapp from "../models/Whatsapp";
 import GetDefaultWhatsAppByUser from "./GetDefaultWhatsAppByUser";
 
 const GetDefaultWhatsApp = async (
+  companyId: number,
   userId?: number
 ): Promise<Whatsapp> => {
   if(userId) {
-    const whatsappByUser = await GetDefaultWhatsAppByUser(userId);
+    const whatsappByUser = await GetDefaultWhatsAppByUser(userId,);
     if(whatsappByUser !== null) {
       return whatsappByUser;
     }
   }
 
   const defaultWhatsapp = await Whatsapp.findOne({
-    where: { isDefault: true }
+    where: { isDefault: true, companyId }
   });
+
   if (!defaultWhatsapp) {
     throw new AppError("ERR_NO_DEF_WAPP_FOUND");
   }
+
   return defaultWhatsapp;
 };
 
